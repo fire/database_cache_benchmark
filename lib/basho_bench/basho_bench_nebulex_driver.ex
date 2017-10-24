55a03e28a09fab6bda1944a85af76bf78e8261e0
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2017-10-24 11:55:23 -0700

    Add insert_or_update.

--- lib/basho_bench/basho_bench_nebulex_driver.ex
+++ lib/basho_bench/basho_bench_nebulex_driver.ex
@@ -3,9 +3,23 @@ defmodule BashoBench.Nebulex.Driver do
     {:ok, {}}
   end
 
-  def run(:put, key_gen, value_gen, state) do
+  def run(:put_on_conflict, key_gen, value_gen, state) do
     changeset = Hello.World.changeset(%Hello.World{}, %{id: key_gen.(), random_number: value_gen.()})
     case Hello.CacheableRepo.insert(changeset, returning: true, on_conflict: [set: [random_number: value_gen.()]], conflict_target: :id) do
+      {:ok, _schema} -> {:ok, state}
+      {:error, reason} -> {:error, reason, state}
+    end
+  end
+
+  def run(:put, key_gen, value_gen, state) do
+    result =
+      case Hello.CacheableRepo.get(Hello.World, key_gen.()) do
+        nil  -> %Hello.World{id: key_gen.(), random_number: value_gen.()}
+        world -> world
+      end
+      |> Hello.World.changeset(%{random_number: value_gen.()})
+      |> Hello.CacheableRepo.insert_or_update
+    case result do
       {:ok, _schema} -> {:ok, state}
       {:error, reason} -> {:error, reason, state}
     end

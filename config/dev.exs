3149d56ae392b1f759c3bab20c58e412d4f648a5
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-05-06 15:16:07 -0700

    Update deps to support ssl.

--- config/dev.exs
+++ config/dev.exs
@@ -4,12 +4,13 @@ config :hello, Hello.Repo,
   adapter: Ecto.Adapters.SnappyData,
   username: "app",
   password: "",
-  hostname: "192.168.0.23",
+  hostname: "192.168.0.10",
   pool: DBConnection.Poolboy,
   pool_size: 20,
   schema: "app",
+  opts: [ssl_opts: [enabled: true, cacertfile: "config/cacerts.pem", certfile: "config/cert.pem", keyfile: "config/key.pem"]],
   # loggers: [{IO, :inspect, []}],
-  port: 32254
+  port: 31002
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"

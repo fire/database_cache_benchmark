ac22ecb5abc8427ff85aba6b8c41c0eef321d565
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-05-06 15:16:27 -0700

    Show an example of a partitioned table.

--- priv/repo/migrations/20170827170943_add_world_table.exs
+++ priv/repo/migrations/20170827170943_add_world_table.exs
@@ -2,7 +2,7 @@ defmodule Hello.Repo.Migrations.AddWorldTable do
   use Ecto.Migration
 
   def up do
-    create table(:world, options: "USING ROW OPTIONS (PERSISTENCE 'ASYNC')") do
+    create table(:world, options: "USING ROW OPTIONS (PARTITION_BY 'PRIMARY KEY', PERSISTENCE 'ASYNC')") do
           add :random_number, :integer, [null: false]
 
           timestamps()

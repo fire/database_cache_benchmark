93db001b58f7b66499e093febd509d76043a90f4
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-08-26 00:33:42 -0700

    Add test.exs to avoid language server errors.

---
+++ config/test.exs
@@ -0,0 +1,16 @@
+use Mix.Config
+
+config :hello, Hello.Repo,
+  adapter: Ecto.Adapters.SnappyData,
+  username: "app",
+  password: "",
+  hostname: "192.168.0.10",
+  pool: DBConnection.Poolboy,
+  pool_size: 40,
+  schema: "app",
+  opts: [ssl_opts: [enabled: true, cacertfile: "config/cacerts.pem", certfile: "config/cert.pem", keyfile: "config/key.pem"]],
+  # loggers: [{IO, :inspect, []}],
+  port: 1528
+
+# Do not include metadata nor timestamps in development logs
+config :logger, :console, format: "[$level] $message\n"

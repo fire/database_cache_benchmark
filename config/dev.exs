549647234c695d16a9d9b4d92d45981ed55b0fed
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2018-09-04 10:01:19 -0700

    Update dev configuration.

--- config/dev.exs
+++ config/dev.exs
@@ -4,13 +4,13 @@ config :hello, Hello.Repo,
   adapter: Ecto.Adapters.SnappyData,
   username: "app",
   password: "",
-  hostname: "192.168.0.10",
+  hostname: "192.168.39.25",
   pool: DBConnection.Poolboy,
   pool_size: 40,
   schema: "app",
-  opts: [ssl_opts: [enabled: true, cacertfile: "config/cacerts.pem", certfile: "config/cert.pem", keyfile: "config/key.pem"]],
+  opts: [ssl_opts: [enabled: false, cacertfile: "config/cacerts.pem", certfile: "config/cert.pem", keyfile: "config/key.pem"]],
   # loggers: [{IO, :inspect, []}],
-  port: 1528
+  port: 32239
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
